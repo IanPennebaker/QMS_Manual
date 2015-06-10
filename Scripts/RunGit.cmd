f9f61a1b54cfe42b7da9f9a7465211e5c3f89c85
IanPennebaker <IanJPennebaker@gmail.com>
2015-06-10 10:08:34 -0700

    revTime

--- Scripts/RunGit.cmd
+++ Scripts/RunGit.cmd
@@ -1,9 +1,7 @@
-cd Desktop\QMS_Manual
+'cd Desktop\QMS_Manual
 git add . -A
 set revTime= "Revision Date:  " + %TIME:~0,2%
 git commit -m revTime
 git push
-IanJPennebaker@gmail.com
-Viewsonic1
 cd ..
 cd ..
